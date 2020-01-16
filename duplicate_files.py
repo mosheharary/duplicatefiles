0c2dd7a11258211ecc575f691d2137aad70e5f8b
Moshe Harary <mosheh@cb4.com>
2020-01-16 10:07:17 +0200

    use os.stat

--- duplicate_files.py
+++ duplicate_files.py
@@ -2,14 +2,29 @@ import sys
 import hashlib
 from collections import defaultdict
 
-def file_as_bytes(file):
-    with file:
-        return file.read()
+def calc_hash(file,check_all):
+
+    BLOCK_SIZE = 65536
+
+    file_hash = hashlib.sha256()
+    with open(file, 'rb') as f:
+        fb = f.read(BLOCK_SIZE)
+        if check_all == False:
+            file_hash.update(fb)
+            return file_hash.hexdigest()
+            #only check calculate the first block of the file
+        while len(fb) > 0:
+            file_hash.update(fb)
+            fb = f.read(BLOCK_SIZE)
+
+    return file_hash.hexdigest()
 
 def list_files_recursive(path):
 
     import os
-    dd_md5 = defaultdict(list)
+    import stat
+    dd_hash = defaultdict(list)
+    dd_hash_part = defaultdict(list)
     dd_size = defaultdict(list)
     total_files=0
 
@@ -19,33 +34,44 @@ def list_files_recursive(path):
         for file in f:
             path=os.path.join(r, file)
             #get the full path of the file
+            if os.access(path, os.R_OK) == False:
+                print ("Can not read " + path)
+                continue
             if os.path.isfile(path) == True:
             #check if it's a file
-                size=os.path.getsize(path)
-                #check size in bytes
+                st = os.stat(path)
+                size=st.st_size
+                #get size in bytes
                 dd_size[size].append(path)
-                #inset to hash - key is the size
-                length=len(dd_size[size])
+                #inset to hash , key is the size
                 total_files += 1
-                if length > 1:
-                    #if we have another file with the same size , caluculate its md5
-                    tmp_file=dd_size[size].pop(0)
-                    #we pop the first file from the hash (so we will not calculate it's md5 twice)
-                    md5=hashlib.md5(file_as_bytes(open(tmp_file, 'rb'))).hexdigest()
-                    dd_md5[md5].append(path)
-                    #put the file in a diffrent hash when the key is the md5
-                    if len(dd_md5[md5]) == 1:
-                        #if , after the append , the size is 1 , we have to inset the first one also (the one that we pop)
-                        dd_md5[md5].append(tmp_file)
 
-#go over the md5 hash files and print the name(full path) of it
+    for key,val in dd_size.items():
+        if len(val) > 1:
+        #check if we have an entry > 1 in size based hash
+            for file in dd_size.get(key):
+                hash=calc_hash(file,False)
+                #calculate file hash based on the first file block only and inset to another hash
+                dd_hash_part[hash].append(file)
+
+    for key,val in dd_hash_part.items():
+        if len(val) > 1:
+        #check if we have an entry > 1 in partial file  based hash
+            for file in dd_hash_part.get(key):
+                hash=calc_hash(file,True)
+                #calculate file hash and inset to another hash
+                dd_hash[hash].append(file)
+
     print ("Scan " + str(total_files) + " files...")
-    print ("duplicate files with the same md5:")
-    for key,val in dd_md5.items():
-        print ("md5:"+key)
-        for file in dd_md5.get(key):
-            print ("\t" + file)
-        #print (dd_md5.get(key))
+    print ("duplicate files with the same hash:")
+    for key,val in dd_hash.items():
+        if len(val) > 1:
+        #print only hash entries > 1
+            print ("hash:"+key)
+            for file in dd_hash.get(key):
+                print ("\t" + file)
+
+
 
 
 if __name__ == '__main__':
